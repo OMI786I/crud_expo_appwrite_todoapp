f6e3616d5835374cefe6ea81bfc8cc905e3ffbc1
OMI786I <sohrawardy1998@gmail.com>
2025-02-06 01:28:26 +0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,76 @@
+import TaskRead from "@/components/TaskRead";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  const [text, onChangeText] = useState("");
+  console.log(text);
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 30,
+              textAlign: "center",
+            }}
+          >
+            Welcome to TODO app made with react Native and appwrite
+          </Text>
+        </View>
+
+        <View>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            placeholder="add a new task"
+          />
+
+          <TouchableOpacity
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 40,
+              backgroundColor: "blue",
+              padding: 10,
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+              }}
+            >
+              {" "}
+              Add Task +
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            marginTop: 40,
+          }}
+        >
+          <TaskRead />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 40,
+  },
+});
---
+++ components/TaskRead.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TaskRead = () => {
+  return (
+    <View>
+      <Text>TaskRead</Text>
+    </View>
+  );
+};
+
+export default TaskRead;
+
+const styles = StyleSheet.create({});
