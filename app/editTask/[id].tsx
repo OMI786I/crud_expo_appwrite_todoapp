b9fbda0a872f1cc338240017323a65c368683050
OMI786I <sohrawardy1998@gmail.com>
2025-02-06 14:44:55 +0600

    added ui for edit task page

--- app/editTask/[id].tsx
+++ app/editTask/[id].tsx
@@ -1,14 +1,119 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const taskId = () => {
+  const [text, onChangeText] = useState("");
+  const [status, setStatus] = useState(false);
   const { id } = useLocalSearchParams();
-  console.log(id);
+
+  const updateData = () => {
+    const toSendData = {
+      tasks: text,
+      complete: status,
+    };
+    console.log(toSendData);
+  };
+
   return (
-    <View>
-      <Text>id: {id}</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 30,
+              textAlign: "center",
+            }}
+          >
+            id: {id}
+          </Text>
+        </View>
+
+        <View>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            placeholder="add a new task"
+          />
+
+          <View
+            style={{
+              flexDirection: "row",
+              columnGap: 10,
+              justifyContent: "center",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 40,
+                backgroundColor: "blue",
+                padding: 10,
+              }}
+              onPress={() => setStatus(true)}
+            >
+              <Text
+                style={{
+                  color: "white",
+                }}
+              >
+                Completed!
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 40,
+                backgroundColor: "blue",
+                padding: 10,
+              }}
+              onPress={() => setStatus(false)}
+            >
+              <Text
+                style={{
+                  color: "white",
+                }}
+              >
+                Not Completed!
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          {status ? <Text>Completed</Text> : <Text>In Complete</Text>}
+
+          <TouchableOpacity
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              marginTop: 40,
+              backgroundColor: "blue",
+              padding: 10,
+            }}
+            onPress={() => updateData()}
+          >
+            <Text
+              style={{
+                color: "white",
+              }}
+            >
+              Update Task
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
